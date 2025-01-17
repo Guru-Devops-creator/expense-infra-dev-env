ecb9d5a36f722b0606e1951d132520d8a4628d25
Guru-Devops-creator <gurut591@gmail.com>
2025-01-17 13:44:43 +0530

    terraform

--- 60-backend/main.tf
+++ 60-backend/main.tf
@@ -41,7 +41,7 @@ resource "null_resource" "backend" {
   provisioner "remote-exec" {
     # Bootstrap script called with private_ip of each node in the cluster
     inline = [
-      "chmod +x /tmp/backend.sh"
+      "chmod +x /tmp/backend.sh",
       "sudo sh /tmp/backend.sh ${var.backend_tags.Component}.${var.environment}"
     ]
   }
